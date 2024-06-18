92adfee36a12d7e5ba6292d1a3214de7ae6e6eb8
Conor <conordmcdevitt@gmail.com>
2024-06-18 18:32:19 -0400

    header

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      {/*Header = login  */}
+      <div className="">
+
+      
+      <div className="flex p-[.5vw]">
+        <h1 className="text-xl font-extrabold roundedxl w-3/4">heatmaps</h1>
+        <button className="w-1/4 ">Login</button>
+      </div>
+      <div className="shadow pr pl-4 rounded-full w-1/1 h-1"></div>
+    </div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="container p-[1vw] rounded center min-h-screen w-1/3">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
