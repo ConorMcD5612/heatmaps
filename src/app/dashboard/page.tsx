92d35512af2c8e6894757f677b82e18fa5aa556a
Conor <conordmcdevitt@gmail.com>
2024-06-23 01:50:18 -0400

    styles

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,10 +11,10 @@ export default function Page() {
       
       <div className="flex p-[.5vw] shadow-[0px_1px_1px_-1px_rgba(0,0,0,1)]">
         <h1 className="text-xl font-extrabold  roundedxl w-3/4">heatmaps</h1>
-        <div className="">
+        <div className="ml-auto">
 
        
-        <button className=" justify-self-end "><FeatherIcon icon="log-in"/></button>
+        <button className=""><FeatherIcon icon="log-in"/></button>
         </div>
       </div>
     </div>
