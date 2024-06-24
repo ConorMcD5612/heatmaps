d75312fd9c26b90a2d7f317eccf20e2ed598035c
Conor <conordmcdevitt@gmail.com>
2024-06-23 23:46:34 -0400

    folder

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 import FeatherIcon from "feather-icons-react";
 
-
 export default function Page() {
   return (
     <div>
-      {/*Header = login  */}
       <div className="">
-
-      
-      <div className="flex p-[.5vw] shadow-[0px_1px_1px_-1px_rgba(0,0,0,1)]">
-        <h1 className="text-xl font-extrabold  roundedxl w-3/4">heatmaps</h1>
-        <div className="ml-auto">
-
-       
-        <button className=""><FeatherIcon icon="log-in"/></button>
+        <div className="flex p-[.5vw] shadow-[0px_1px_1px_-1px_rgba(0,0,0,1)]">
+          <h1 className="text-xl font-extrabold  roundedxl w-3/4">heatmaps</h1>
+          <div className="ml-auto">
+            <button className="">
+              <FeatherIcon icon="log-in" />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
---
+++ src/ui/heatmaps/heatmap.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const heatmap = () => {
+  return (
+    <div>heatmap</div>
+  )
+}
