ad0591dab880945d9922720f585855e6e5cf7b83
Conor <conordmcdevitt@gmail.com>
2024-07-11 19:02:45 -0400

    import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./components/Provider";
-import LoginBtn from "./components/loginBtn";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
