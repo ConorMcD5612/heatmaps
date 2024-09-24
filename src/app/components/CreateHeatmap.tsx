a25c78a5d69c7b4a5d57a602217e15d2cce2b4c0
Conor <conordmcdevitt@gmail.com>
2024-09-23 22:55:55 -0400

    heatmap create styles

--- src/app/components/CreateHeatmap.tsx
+++ src/app/components/CreateHeatmap.tsx
@@ -6,10 +6,11 @@ export default function CreateHeatmap() {
 
 
   return (
-    <div className='border-2 opacity-50 border-black border-dashed flex justify-center p-1'>
-      <div className="opacity-100">
-      <FeatherIcon  size={30} icon='plus-circle'/>
-      </div>
+    <div className='border-2 opacity-50 border-black border-dashed flex gap-1 justify-center p-1'>
+      
+      <FeatherIcon className="my-auto" size={20} icon='plus-circle'/>
+
+      <h4 className='my-auto'>Add Heatmap</h4>
     </div>
   )
 }
