52ae6e315ca72986276b83cb1321538c5f007dac
Conor <conordmcdevitt@gmail.com>
2024-09-23 22:36:08 -0400

    button style

--- src/app/components/CreateHeatmap.tsx
+++ src/app/components/CreateHeatmap.tsx
@@ -6,8 +6,10 @@ export default function CreateHeatmap() {
 
 
   return (
-    <div className='border-4 border-black border-dashed'>
+    <div className='border-2 opacity-50 border-black border-dashed flex justify-center p-1'>
+      <div className="opacity-100">
       <FeatherIcon  size={30} icon='plus-circle'/>
+      </div>
     </div>
   )
 }
