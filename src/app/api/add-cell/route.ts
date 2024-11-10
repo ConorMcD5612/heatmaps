08a93d9794586a9d63f910c650e6ea572f21262a
Conor <conordmcdevitt@gmail.com>
2024-11-09 20:29:20 -0500

    nextresponse returning in api call

--- src/app/api/add-cell/route.ts
+++ src/app/api/add-cell/route.ts
@@ -5,8 +5,12 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
     //
     try {
-        await sql`INSERT INTO cell_data `
+        let result = await sql`INSERT INTO cell_data `
     } catch (error) {
-
+        return NextResponse.json({error}, {status: 500})
     }
+
+    return NextResponse.json({}, {status: 500})
 }
+    
+   
