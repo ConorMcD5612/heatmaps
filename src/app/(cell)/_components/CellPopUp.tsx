81e19f4ebfebac0f174c850057a2b40d148dda81
Conor <conordmcdevitt@gmail.com>
2024-12-31 20:19:25 -0500

    cellpopup centered

--- src/app/(cell)/_components/CellPopUp.tsx
+++ src/app/(cell)/_components/CellPopUp.tsx
@@ -2,29 +2,26 @@ import React from "react";
 import { clsx } from "clsx";
 import dateToYYYYMMDD from "@/app/lib/utils";
 
-
 export default function CellPopUp({
   date,
   timeMins,
-  mapName
+  mapName,
 }: {
   date: Date;
   timeMins: number;
   mapName: string;
 }) {
-  const formattedDate = dateToYYYYMMDD(date)
+  const formattedDate = dateToYYYYMMDD(date);
 
   return (
     <>
-        <div
-          className={"absolute block top-[-110%] left-[-100%] rounded bg-black p-1 text-white z-10 invisible group-hover/item:visible hover:invisible text-xs whitespace-nowrap"
-          }
-        >
-         
-            {`${timeMins} minutes of ${mapName} on ${formattedDate}`}
-          
-        </div>
-   
+      <div
+        className={
+          "absolute block top-[-115%] left-1/2 transform -translate-x-1/2 rounded bg-black p-1 text-white z-10 invisible group-hover/item:visible hover:invisible text-xs whitespace-nowrap  "
+        }
+      >
+        {`${timeMins} minutes of ${mapName} on ${formattedDate}`}
+      </div>
     </>
   );
 }
