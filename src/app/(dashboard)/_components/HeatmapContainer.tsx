06179f2e2a3916d9491a1db2ea318e347ef81f6d
Conor <conordmcdevitt@gmail.com>
2025-04-24 17:26:17 -0400

    fixed type error

--- src/app/(dashboard)/_components/HeatmapContainer.tsx
+++ src/app/(dashboard)/_components/HeatmapContainer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { fetchHeatmapData } from "../../lib/data";
 import Heatmap from "../../(heatmap)/_components/Heatmap";
-import { HeatmapData } from "../../lib/definitions";
+import { HeatmapData, HeatmapParsed } from "../../lib/definitions";
 import CreateHeatmapBtn from "./CreateHeatmapBtn";
 
 
@@ -9,7 +9,7 @@ export default async function HeatmapContainer() {
   const heatmapData = await fetchHeatmapData();
   return (
     <div className="h-full">
-      {heatmapData.map((data: HeatmapData) => (
+      {heatmapData.map((data: HeatmapParsed) => (
         <Heatmap 
         key={data.heatmap_id}
         heatmapData={data}
